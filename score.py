047422367b43c9342166cfd75416acd0cf89d1a1
Talal Widatalla <63631602+twidatalla@users.noreply.github.com>
2024-05-21 10:54:17 -0700

    Added structure loading and tqdm

--- score.py
+++ score.py
@@ -1,20 +1,20 @@
 import esm
 import torch
 import pandas as pd
-from esm.inverse_folding.util import score_sequence, get_sequence_loss
+from esm.inverse_folding.util import score_sequence, get_sequence_loss, load_structure, extract_coords_from_structure
 import numpy as np
 import argparse
 import os
+from tqdm import tqdm
 
 #args
 parser = argparse.ArgumentParser(description='Training script with command-line arguments.')
 parser.add_argument('--weights_path', type=str, required=False, help='path to model weights')
 parser.add_argument('--dataset_path', type=str, required=True, help='path to csv containing sequences/mutations')
-parser.add_argument('--feature', type=str, default='ddG_ML', required = False, help='name of fitness value')
+parser.add_argument('--feature', type=str, required = False, help='name of fitness value')
 parser.add_argument('--normalize', action = 'store_true', help = 'whether or not to normalize by subtracting wild-type')
 parser.add_argument('--whole_seq', action = 'store_true', help = 'whether or not to use whole seq instead of mut pos')
 parser.add_argument('--sum', action = 'store_true', help = 'if scoring whole seq whether or not to sum likelihoods instead of average')
-parser.add_argument('--pdb_dir', type=str, default='data/af_pdbs/', required = False, help='directory containing pdbs')
 parser.add_argument('--out_path', type=str, default='results.csv', required = False)
 args = parser.parse_args()
 
@@ -44,7 +44,9 @@ model.eval()
 alphabet = model.decoder.dictionary
 pdb_grouped = dataset.groupby('WT_name')
 
-for pdb_path, group_df in (pdb_grouped, total = len(pdb_grouped)):
+for pdb_path, group_df in tqdm(pdb_grouped, total = len(pdb_grouped)):
+    structure = load_structure(pdb_path)
+    coord, _= extract_coords_from_structure(structure)
     for seq, feature, muts in zip(group_df['aa_seq'], group_df[args.feature], group_df['mut_type']):
         mutation_info = [(mut[0], int(mut[1:-1]), mut[-1]) for mut in muts.split(':') if muts != 'wt']
         
